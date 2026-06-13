ed11492825e91f956140ec9993ba91470a8eadfa
Mohit Chaudhary <mohitchaudharyraya0101@gmail.com>
2026-06-13 07:17:53 +0530

    Add files via upload

---
+++ Day-5/Q19.c
@@ -0,0 +1,20 @@
+//Write a program to Print factors of a number.
+#include <stdio.h>
+
+int main() {
+    int num;
+
+    printf("Enter a number: ");
+    scanf("%d", &num);
+
+    printf("Factors of %d are: ", num);
+
+    for(int i = 1; i <= num; i++) {
+        if(num % i == 0) {
+            printf("%d ", i);
+        }
+    }
+
+    printf("\n");
+    return 0;
+}
---
+++ Day-5/Q20.c
@@ -0,0 +1,31 @@
+//Write a program to Find largest prime factor.
+#include <stdio.h>
+
+// Function to check if a number is prime
+int isPrime(int n) {
+    if (n <= 1) return 0;
+    for (int i = 2; i * i <= n; i++) {
+        if (n % i == 0) return 0;
+    }
+    return 1;
+}
+
+int main() {
+    int num, largestPrime = -1;
+
+    printf("Enter a number: ");
+    scanf("%d", &num);
+
+    for (int i = 2; i <= num; i++) {
+        if (num % i == 0 && isPrime(i)) {
+            largestPrime = i;
+        }
+    }
+
+    if (largestPrime != -1)
+        printf("Largest prime factor of %d is %d\n", num, largestPrime);
+    else
+        printf("%d has no prime factors.\n", num);
+
+    return 0;
+}
