7a93a57ed2c88d5c07761e229b3b4b74293f4544
Mohit Chaudhary <mohitchaudharyraya0101@gmail.com>
2026-07-01 07:41:34 +0530

    Add files via upload

---
+++ Day-23/Q92.c
@@ -0,0 +1,31 @@
+// Q92	Write a program to Find maximum occurring character.
+
+
+#include <stdio.h>
+
+int main() {
+    char str[100];
+    int freq[256] = {0};
+    int i, max = 0;
+    char ch;
+
+    printf("Enter a string: ");
+    fgets(str, sizeof(str), stdin);
+
+    for (i = 0; str[i] != '\0'; i++) {
+        if (str[i] != '\n')
+            freq[(unsigned char)str[i]]++;
+    }
+
+    for (i = 0; i < 256; i++) {
+        if (freq[i] > max) {
+            max = freq[i];
+            ch = (char)i;
+        }
+    }
+
+    printf("Maximum occurring character: %c\n", ch);
+    printf("Frequency: %d\n", max);
+
+    return 0;
+}
